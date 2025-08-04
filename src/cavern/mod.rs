79669da0e1925dc428c53dcfe1e269af869b145a
McShinyShoe <mcshinyshoe@protonmail.com>
2025-08-04 21:31:41 +0700

    feat: Added parser for marker information

---
+++ src/cavern/mod.rs
@@ -0,0 +1,1 @@
+pub mod towny_info;
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ use reqwest::Error;
 mod dynmap;
 use dynmap::players::get_online_players;
 use dynmap::settings::get_setting;
-use dynmap::markers::get_marker_groups;
+
+mod cavern;
+use cavern::towny_info::get_towny_info;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -15,7 +17,7 @@ async fn main() -> Result<(), Error> {
     let data = get_setting().await?;
     println!("{:#?}", &data);
     
-    let data= get_marker_groups().await?;
+    let data= get_towny_info().await?;
     println!("{:#?}", &data);
 
     Ok(())
