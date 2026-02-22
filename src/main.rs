87d04c8bacde572d5db7e4a9099e378693a02f58
McShinyShoe <mcshinyshoe@protonmail.com>
2026-02-22 14:22:25 +0700

    feat: Cleaned main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,8 @@
 #![allow(dead_code)]
 
-mod dynmap;
-use dynmap::players::get_online_players;
-use dynmap::settings::get_setting;
-
-mod cavern;
-use cavern::towny_info::get_towny_info;
-
 use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let data = get_online_players().await?;
-    println!("{:#?}", &data);
-
-    let data = get_setting().await?;
-    println!("{:#?}", &data);
-    
-    let data= get_towny_info().await?;
-    println!("{:#?}", &data);
-
     Ok(())
 }
